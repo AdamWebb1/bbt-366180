6a6f02981e7c10a8c2b69891620395f6d3f97db6
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-11-01 15:49:58 -0700

    Added word-in-title/:word route

--- functions/router.js
+++ functions/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const episodes = require('./routes/episodes');
 const episodeIndex = require('./routes/episodeIndex');
+const inTitle = require("./routes/inTitle")
 
 router.get("/episodes", episodes);
 router.get("/episode-index/:index", episodeIndex)
+router.get("/word-in-title/:word", inTitle)
 
 module.exports = router;
---
+++ functions/routes/inTitle.js
@@ -0,0 +1,28 @@
+const bbt = require("big-bang-theory")
+
+
+
+function hasWord(episode, word){
+    return bbt._embedded.episodes[episode].name.toLowerCase().indexOf(word) != -1
+}
+
+function generateResp(word){
+    response = []
+
+    for (episode in bbt._embedded.episodes){
+        if (hasWord(episode, word)){
+            response.push( bbt._embedded.episodes[episode])
+        }
+    }
+    console.log(response)
+    return response
+}
+
+const episodes = (req, res) => {
+    res.status(200).json({
+      success: true,
+      data: generateResp(req.params.word)
+    })
+  }
+  
+module.exports = episodes;
